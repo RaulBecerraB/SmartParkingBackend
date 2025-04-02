c2a0204f0b90c1970cfa792f73c30a432e0fa8ed
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-04-02 08:14:29 -0600

    refactor(Program.cs):Add static connectionString

--- obj/Debug/net8.0/SmartParkingBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartParkingBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartParkingBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744d5d670be8ba11dca9c82731ac8331e6febccc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2c37ca143b5b4f5637a398fc1052067722c3eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartParkingBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartParkingBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
